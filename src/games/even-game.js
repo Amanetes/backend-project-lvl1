7619cd09d51584e17f70b7d01620d64dbe3a0491
Sergey Fatyukhin <ser_8904@mail.ru>
2022-01-10 20:33:57 +0300

    add brain-even

---
+++ src/games/even-game.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+// рандомизатор чисел
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+// инициализация правил и количества раундов
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const roundCount = 3;
+
+const startEvenGame = () => {
+  console.log('Welcome to the Brain Games');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(gameRules);
+  // зацикливание процесса до 3х раундов
+  for (let i = 1; i <= roundCount; i += 1) {
+    const number = randomNumber(0, 100);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer:');
+    if (number % 2 === 0 && answer === 'yes') {
+      console.log('Correct!');
+    } else if (number % 2 !== 0 && answer === 'no') {
+      console.log('Correct!');
+    } else if (number % 2 === 0 && answer === 'no') {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes')`);
+      return console.log(`Let's try again, ${name}`);
+    } else if (number % 2 !== 0 && answer === 'yes') {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no')`);
+      return console.log(`Let's try again, ${name}`);
+    }
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
+export default startEvenGame;
