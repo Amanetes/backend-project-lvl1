cf7c74f605408ba6e64a84c77b8c8ceee19ead05
Sergey Fatyukhin <ser_8904@mail.ru>
2022-01-11 17:00:28 +0300

    update even-game

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,34 +1,28 @@
-import readlineSync from 'readline-sync';
-// рандомизатор чисел
-const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-// инициализация правил и количества раундов
-const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
-const roundCount = 3;
+import { gameRoundGenerator, getRandomNumber } from '../index.js';
+// Правила игры
 
-const startEvenGame = () => {
-  console.log('Welcome to the Brain Games');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log(gameRules);
-  // зацикливание процесса до 3х раундов
-  for (let i = 1; i <= roundCount; i += 1) {
-    const number = randomNumber(0, 100);
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer:');
-    if (number % 2 === 0 && answer === 'yes') {
-      console.log('Correct!');
-    } else if (number % 2 !== 0 && answer === 'no') {
-      console.log('Correct!');
-    } else if (number % 2 === 0 && answer === 'no') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes')`);
-      console.log(`Let's try again, ${name}`);
-      return;
-    } else if (number % 2 !== 0 && answer === 'yes') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no')`);
-      console.log(`Let's try again, ${name}`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+// Проверка четности
+const isEven = (number) => (number % 2 === 0);
+
+// Описание логики работы игры: генерация случайного числа и проверка четности
+
+const gameLogic = () => {
+  // Генерация случайного числа в вопросе
+  const number = getRandomNumber(0, 100);
+  const gameQuestion = `${number}`;
+  const gameResult = [];
+  // Условие получения правильного ответа
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  // Возврат правильного ответа и вопроса из функции
+  gameResult.push(gameQuestion, correctAnswer);
+  return gameResult;
 };
+// Функция запуска игры, которая возвращает генератор раундов
+// Генератор в свою очередь принимает правила, которые выводит в консоль
+// И логику игры
+
+const startEvenGame = () => gameRoundGenerator(gameRules, gameLogic);
+
 export default startEvenGame;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+
+// number randomizer
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const roundCount = 3;
+// Генератор раундов, взаимодействие с пользователем
+const gameRoundGenerator = (gameRules, gameLogic) => {
+  console.log('Welcome to the Brain Games');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(gameRules);
+
+  for (let i = 1; i <= roundCount; i += 1) {
+    const [gameQuestion, correctAnswer] = gameLogic();
+    console.log(`Question: ${gameQuestion}`);
+    const userAnswer = readlineSync.question('Your answer:');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}`);
+};
+
+export { getRandomNumber, gameRoundGenerator };
