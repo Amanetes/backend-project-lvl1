32bd018b4c8640131e677f4aa2f55cc100a9bd7a
Sergey Fatyukhin <ser_8904@mail.ru>
2022-01-12 17:02:59 +0300

    add brain-prime game

---
+++ src/games/prime-game.js
@@ -0,0 +1,33 @@
+import { gameRoundGenerator, getRandomNumber } from '../index.js';
+
+// Правила игры
+
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+// Определение функции простоты числа
+// делится без остатка только на 1 и само на себя
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let divisor = 2; divisor <= (number / 2); divisor += 1) {;
+    if (number % divisor === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const gameLogic = () => {
+  const number = getRandomNumber(0, 100);
+  const gameQuestion = `${number}`;
+  const gameResult = [];
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  gameResult.push(gameQuestion, correctAnswer);
+  return gameResult;
+};
+
+const startProgGame = () => gameRoundGenerator(gameRules, gameLogic);
+
+export default startProgGame;
